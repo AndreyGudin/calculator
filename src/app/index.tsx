46bac780991c143bdd4711c4bd8cb8b37af983fc
Andrey Gudin <aeon150791@gmail.com>
2023-03-08 15:06:52 +0400

    feat: add modeSwitcher

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Switcher } from '../entities/modeSwitcher/ui';
 import { Button, ThemeButton } from '../shared/ui/Button/Button';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
       <Button theme={ThemeButton.OPERATION}>1</Button>
+      <Switcher />
     </div>
   );
 }
